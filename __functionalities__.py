cb1341d9d44ab5144614c071739418dabe2690ee
TheyCallMeRZ <przemko328@gmail.com>
2025-11-09 16:23:38 +0100

    moving forward

--- __functionalities__.py
+++ __functionalities__.py
@@ -3,9 +3,17 @@ import requests
 
 def get_book_list():
     url = "https://www.gutenberg.org/cache/epub/feeds/pg_catalog.csv"
-    list_name = 'assets/gutenberg_book_list.txt'
+    list_name = 'assets/gutenberg_book_list.csv'
     response = requests.get(url)
     with open(list_name, 'wb') as f:
         f.write(response.content)
 
-get_book_list()
+def download_book(book_id, filename=''):
+    
+    pass
+
+def title_helper(title):
+    pass
+
+def jaro():
+    pass
